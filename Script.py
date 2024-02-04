9491a70ae3b3ad5678f0249ee6282bcb32882103
nithish01k <155215189+nithish01k@users.noreply.github.com>
2024-02-04 22:57:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -209,7 +209,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ :@tamil_linkz1" {file_name}
+    CAPTION = """<@tamil_linkz1>📂Fɪʟᴇɴᴀᴍᴇ :@tamil_linkz1 {file_name}
 
 <b> Size ⚙️: {file_size}</b>""" 
 
